a819afe5a585942550b68920057f3db66b867e1d
Finally-Kwaku <kwakunagyamfi@gmail.com>
2022-12-09 15:09:54 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,13 +11,9 @@ int main(void)
 	char lc;
 
 	for (num = 0; num <= 10; num++)
-	{
 		putchar((num % 10) + '0');
-	}
 	for (lc + 'a'; lc <= 'f'; lc++)
-	{
 		putchar(lc);
-	}
 	putchar('\n');
 	return (0);
 }
