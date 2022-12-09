2249d5e30633b418b57a3b3bf3051f6889b1db48
Finally-Kwaku <kwakunagyamfi@gmail.com>
2022-12-09 14:03:04 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers
+ * from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
