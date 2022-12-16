b52512266bb88b7b13bb651f991bf5d4741522a7
Finally-Kwaku <kwakunagyamfi@gmail.com>
2022-12-15 19:36:01 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,33 +9,28 @@
  */
 int main(void)
 {
-	int p = 100;
-	int i;
+	int n;
 
-	i = 1;
-	while (i <= p)
+	n = 1;
+	printf("%d", n)
+	for (n = 2; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 3 == 0)
+		else if (n % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (n % 5 == 0)
 		{
-			if (i < p)
-			printf("Buzz ");
-
-			else
 			printf("Buzz");
 		}
 		else
 		{
-			printf("%i ", i);
+			printf(" %d", n);
 		}
-		i++;
 	}
 	printf("\n");
 	return (0);
