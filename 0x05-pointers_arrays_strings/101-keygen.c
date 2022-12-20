6b26ee7e779d21ce006977dd46a0730dd5e05a9a
Finally-Kwaku <kwakunagyamfi@gmail.com>
2022-12-19 16:11:26 -0800

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,25 +11,17 @@
 
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
-
-	sum = 0;
+	int pass, sum;
 
 	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
+	sum = 0;
+	while (sum <= 2645)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] = '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
 	}
+	printf("%c", 2772 - sum);
+
 	return (0);
 }
