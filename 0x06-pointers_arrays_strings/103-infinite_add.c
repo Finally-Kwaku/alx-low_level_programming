7e89a3c8ffab81af9f04b250044ecd65e55eac35
Finally-Kwaku <kwakunagyamfi@gmail.com>
2022-12-22 15:59:32 -0800

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
-char *infinte_add(char *n1, char *n2, char *r, int size_r);
-
 /**
  * add_strings - Adds the numbers stored in two strings
  * @n1: the string containing the first number to be added
